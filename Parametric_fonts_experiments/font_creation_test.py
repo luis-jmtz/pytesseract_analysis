6ad502a25321f53b1772dfddfd368b323a29bc47
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-20 13:06:56 -0800

    created full alphabet using svgs

--- Parametric_fonts_experiments/font_creation_test.py
+++ Parametric_fonts_experiments/font_creation_test.py
@@ -8,15 +8,21 @@ os.makedirs(output_folder, exist_ok=True)
 # Define characters to create SVGs for
 alphanumeric_chars = [chr(i) for i in range(48, 58)] + [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
 
-# Function to create an SVG for a character
-def create_svg(character, folder):
-    dwg = svgwrite.Drawing(filename=os.path.join(folder, f"{character}.svg"), size=("100px", "100px"))
+# Function to create an SVG with proper naming convention
+def create_svg_with_naming(character, folder):
+    if character.isupper():
+        filename = f"upper_{character}.svg"
+    elif character.islower():
+        filename = f"lower_{character}.svg"
+    else:
+        filename = f"{character}.svg"  # For numbers
+    dwg = svgwrite.Drawing(filename=os.path.join(folder, filename), size=("100px", "100px"))
     dwg.add(dwg.text(character, insert=("50%", "50%"), text_anchor="middle", alignment_baseline="middle",
                      font_size="72px", font_family="Arial", fill="black"))
     dwg.save()
 
-# Generate SVGs for all alphanumeric characters
+# Generate SVGs for all alphanumeric characters with proper naming
 for char in alphanumeric_chars:
-    create_svg(char, output_folder)
+    create_svg_with_naming(char, output_folder)
 
 output_folder
