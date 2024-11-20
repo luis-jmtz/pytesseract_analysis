3d095712a5b8a9f58f43e40baeac29cee5c83db3
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-20 13:55:14 -0800

    saving just in case

--- Parametric_fonts_experiments/font_creation.py
+++ Parametric_fonts_experiments/font_creation.py
@@ -1,20 +1,15 @@
 import os
 import svgwrite
 
-print("Imports Added")
 def generate_alphanumeric_svgs(weight_value, slant_value, width_value, curviness_value):
 
     # Define alphanumeric characters
     alphanumeric_chars = [chr(i) for i in range(48, 58)] + [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
 
-    print("Charaters Defined")
-
     # Define output folder naming convention
     folder_name = f"weight{weight_value}_slant{slant_value}_width{width_value}_curviness{curviness_value}"
     output_folder = os.path.join(folder_name)
     os.makedirs(output_folder, exist_ok=True)
-
-    print("Folder Named")
 
     # Function to create SVGs for alphanumeric characters
     def create_character_svg(character, folder):
@@ -41,7 +36,12 @@ def generate_alphanumeric_svgs(weight_value, slant_value, width_value, curviness
     for char in alphanumeric_chars:
         create_character_svg(char, output_folder)
 
+"""
+Parameters:
+    weight_value (int): Line thickness.
+    slant_value (int): Font slant in degrees (-ve for backward, +ve for forward).
+    width_value (float): Adjusts the proportions of counters, strokes, spacing, and kerning.
+    curviness_value (int): Level of curviness (0 is angular, higher values are more curved).
+"""
 
-# Example of usage
-generate_alphanumeric_svgs(700, 10, 1.2, 5)
-# "weight700_slant10_width1.2_curviness5"  # Example output folder for reference
+generate_alphanumeric_svgs(700, 0, 0, 0)
