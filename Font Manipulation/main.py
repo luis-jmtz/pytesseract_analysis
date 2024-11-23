78f135e1af62db58cf0c4c491a46d1c570ee2c6b
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-23 12:44:44 -0800

    All pictures for font size, number of images and image spacing created

--- Font Manipulation/main.py
+++ Font Manipulation/main.py
@@ -3,10 +3,10 @@ from letter_image_generator import LetterImageGenerator
 
 generator = LetterImageGenerator(
         letter="A",
-        num_letters=2,
-        letter_spacing=1,
+        num_letters=4,
+        letter_spacing=4,
         num_iterations=50,
-        output_folder="letters2_space1"
+        output_folder="letters4_space4"
     )
 
 generator.generate_images()
