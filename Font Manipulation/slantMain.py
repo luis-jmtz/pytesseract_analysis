d6a4d86f70162384749d119cb7e7d4469dbaa5f0
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-25 18:30:00 -0800

    the code doesn't work with more extreme values

--- Font Manipulation/slantMain.py
+++ Font Manipulation/slantMain.py
@@ -1,14 +1,17 @@
 from Slant_image import TextSlanter
 
+
+slant_value = -60
+image_name = f"{slant_value}.png"
+
 # Define input and output image paths
 input_image_path = "Control_Image.png"
-output_image_path_positive = "positive_slant.png"
 
 # Create an instance of TextItalicizer
-italicizer = TextSlanter(input_image_path, output_image_path_positive)
+italicizer = TextSlanter(input_image_path, image_name)
 
 # Apply a positive slant
-italicizer.italicize_text(slant_factor=20)
+italicizer.italicize_text(slant_factor=slant_value)
 
 # For a negative slant
 italicizer.output_image_path = "negative_slant.png"
