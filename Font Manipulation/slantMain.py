ef1dc68ed7b0b0851286b9f5ebaf997f4b8bafff
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-25 19:21:21 -0800

    made all of the images

--- Font Manipulation/slantMain.py
+++ Font Manipulation/slantMain.py
@@ -1,16 +1,16 @@
 from Slant_image import TextSlanter
 
-slant_value = -65
+slant_value = -60
 input_image_path = "Control_Image.png"
 slanter = TextSlanter(input_image_path, "temp")
 
-image_name = fr"Slanted_Images\{slant_value}_Slant.png"
-slanter.output_image_path = image_name
-slanter.italicize_text(slant_value)
+# image_name = fr"Slanted_Images\{slant_value}_Slant.png"
+# slanter.output_image_path = image_name
+# slanter.italicize_text(slant_value)
 
 
-# while slant_value < 61:
-#     image_name = fr"Slanted_Images\{slant_value}_Slant.png"
-#     slanter.output_image_path = image_name
-#     slanter.italicize_text(slant_value)
-#     slant_value += 1
+while slant_value < 61:
+    image_name = fr"Slanted_Images\{slant_value}_Slant.png"
+    slanter.output_image_path = image_name
+    slanter.italicize_text(slant_value)
+    slant_value += 1
