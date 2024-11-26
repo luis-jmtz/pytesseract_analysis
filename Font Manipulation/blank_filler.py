0d3bc343067b347f0c839eda8fe9feab804470a2
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-25 19:30:36 -0800

    transparency issue fixed for large dataset

--- Font Manipulation/blank_filler.py
+++ Font Manipulation/blank_filler.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 
 # Path to the folder containing the images
-folder_path = "test_slant_folder"
+folder_path = "Slanted_Images"
 
 # Loop through all files in the folder
 for filename in os.listdir(folder_path):
