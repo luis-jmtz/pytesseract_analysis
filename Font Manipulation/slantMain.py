053ff9c78407ee5e9be45cf0fa4bd87a93c81bb9
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-25 19:19:32 -0800

    range has to be between 60 and = -60 or it breaks the code.

--- Font Manipulation/slantMain.py
+++ Font Manipulation/slantMain.py
@@ -1,14 +1,16 @@
 from Slant_image import TextSlanter
 
-slant_value = -60
-#Slanted_Images\Control_Image.png
-image_name = fr"Slanted_Images\{slant_value}_Slant.png"
-
-# Define input and output image paths
+slant_value = -65
 input_image_path = "Control_Image.png"
-
 slanter = TextSlanter(input_image_path, "temp")
 
-
+image_name = fr"Slanted_Images\{slant_value}_Slant.png"
 slanter.output_image_path = image_name
 slanter.italicize_text(slant_value)
+
+
+# while slant_value < 61:
+#     image_name = fr"Slanted_Images\{slant_value}_Slant.png"
+#     slanter.output_image_path = image_name
+#     slanter.italicize_text(slant_value)
+#     slant_value += 1
