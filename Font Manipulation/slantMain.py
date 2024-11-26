a7fbd4f0c920d0cbd83ff40adfd7c437a0971936
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-25 19:14:22 -0800

    dynamic file paths working

--- Font Manipulation/slantMain.py
+++ Font Manipulation/slantMain.py
@@ -1,22 +1,14 @@
 from Slant_image import TextSlanter
 
-
-slant_value = 60
-iteration = 3
-image_name = f"{slant_value}_Slant{iteration}.png"
+slant_value = -60
+#Slanted_Images\Control_Image.png
+image_name = fr"Slanted_Images\{slant_value}_Slant.png"
 
 # Define input and output image paths
 input_image_path = "Control_Image.png"
 
-# Create an instance of TextItalicizer
 slanter = TextSlanter(input_image_path, "temp")
 
-
-slanter.output_image_path = image_name
-slanter.italicize_text(slant_value)
-
-slant_value = -60
-image_name = f"{slant_value}_Slant{iteration}.png"
 
 slanter.output_image_path = image_name
 slanter.italicize_text(slant_value)
