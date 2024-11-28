b24e177afa33291ca45e5d9299895405f9d38338
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-27 17:04:16 -0800

    going to run the loop, hope it doesn't suck

--- Main/main_perlin.py
+++ Main/main_perlin.py
@@ -1,9 +1,19 @@
 from perlin_generator import perlinNoiser
 
-# Initialize and process the image
-processor = perlinNoiser(
-    input_image_path="Control_Image.png",
-    intensity=0.5,
-    output_image_path="output.png"
-)
-processor.process_image()
+changing_intensity = 0.01
+
+while changing_intensity <= 0.1:
+    processor = perlinNoiser("Control_Image.png", changing_intensity,
+    fr"perlin_images\{changing_intensity}.png")
+
+    processor.process_image()
+    
+    changing_intensity += 0.01
+    
+while changing_intensity <= 1:
+    processor = perlinNoiser("Control_Image.png", changing_intensity,
+    fr"perlin_images\{changing_intensity}.png")
+
+    processor.process_image()
+    
+    changing_intensity += 0.1
