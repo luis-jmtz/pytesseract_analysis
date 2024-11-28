bcb08dbd13ef6767c5657b2c00b24a2988c8dcb3
luis-jmtz <luis.jmtz2002@gmail.com>
2024-11-27 18:37:04 -0800

    perlin images made

--- Main/main_perlin.py
+++ Main/main_perlin.py
@@ -1,19 +1,29 @@
 from perlin_generator import perlinNoiser
 
-changing_intensity = 0.01
+changing_intensity = 0.9
 
-while changing_intensity <= 0.1:
-    processor = perlinNoiser("Control_Image.png", changing_intensity,
-    fr"perlin_images\{changing_intensity}.png")
 
-    processor.process_image()
-    
-    changing_intensity += 0.01
-    
-while changing_intensity <= 1:
-    processor = perlinNoiser("Control_Image.png", changing_intensity,
-    fr"perlin_images\{changing_intensity}.png")
+processor = perlinNoiser("Control_Image.png", changing_intensity,
+fr"perlin_images\{changing_intensity}.png")
 
-    processor.process_image()
+processor.process_image()
+
+
+# while changing_intensity <= 0.1:
+#     processor = perlinNoiser("Control_Image.png", changing_intensity,
+#     fr"perlin_images\{changing_intensity}.png")
+
+#     processor.process_image()
     
-    changing_intensity += 0.1
+#     changing_intensity += 0.01
+
+# changing_intensity = 0.1
+
+
+# while changing_intensity <= 1:
+#     processor = perlinNoiser("Control_Image.png", changing_intensity,
+#     fr"perlin_images\{changing_intensity}.png")
+
+#     processor.process_image()
+    
+#     changing_intensity += 0.1
