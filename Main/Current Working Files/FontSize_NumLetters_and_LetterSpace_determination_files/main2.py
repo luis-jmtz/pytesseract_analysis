f982d3f3f458629947275a8bf739b16fa3779fd8
luis-jmtz <luis.jmtz2002@gmail.com>
2024-12-02 22:36:25 -0800

    misc

--- Main/Current Working Files/FontSize_NumLetters_and_LetterSpace_determination_files/main2.py
+++ Main/Current Working Files/FontSize_NumLetters_and_LetterSpace_determination_files/main2.py
@@ -5,12 +5,18 @@ import pytesseract
 from PIL import Image
 import pandas as pd
 
-generator = ImageGenerator(
-        letter="A",
-        num_letters=4,
-        letter_spacing=4,
-        font_size= 20,
-        output_folder="letters4_space4"
-    )
+num_letters = 1
 
-generator.generate_images()
+while num_letters < 20:
+    
+    generator = ImageGenerator(
+            letter="A",
+            num_letters= num_letters,
+            letter_spacing=0,
+            font_size= 20,
+            output_folder="space_tester"
+        )
+
+    generator.generate_images()
+    
+    num_letters += 1
