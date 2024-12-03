0cc03ea57639bca92e9b9534470c49d58e913e6a
luis-jmtz <luis.jmtz2002@gmail.com>
2024-12-03 07:13:39 -0800

    Space and Number Images created

--- Main/Current Working Files/FontSize_NumLetters_and_LetterSpace_determination_files/main2.py
+++ Main/Current Working Files/FontSize_NumLetters_and_LetterSpace_determination_files/main2.py
@@ -6,17 +6,38 @@ from PIL import Image
 import pandas as pd
 
 num_letters = 1
+letter_space = 0
 
-while num_letters < 20:
+# while num_letters < 20:
     
-    generator = ImageGenerator(
-            letter="A",
-            num_letters= num_letters,
-            letter_spacing=0,
-            font_size= 20,
-            output_folder="space_tester"
-        )
+#     generator = ImageGenerator(
+#             letter="A",
+#             num_letters= num_letters,
+#             letter_spacing= letter_space,
+#             font_size= 20,
+#             output_folder= r"Space and Number\f1"
+#         )
 
-    generator.generate_images()
+#     generator.generate_images()
     
-    num_letters += 1
+#     num_letters += 1
+
+
+while letter_space <7:
+    
+    while num_letters < 21:
+    
+        generator = ImageGenerator(
+                letter="A",
+                num_letters= num_letters,
+                letter_spacing= letter_space,
+                font_size= 20,
+                output_folder= fr"Space and Number\space{letter_space}"
+            )
+
+        generator.generate_images()
+        
+        num_letters += 1
+        
+    letter_space +=1
+    num_letters = 1
